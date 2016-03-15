739cfeac77f2d9f3d264686e0466b0b076959c0c
Grgrgrrr <annr29@yandex.ru>
2016-03-15 15:15:55 +0000

    Lesson1 done

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,7 +1,45 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
-var formattedName = HTMLheaderName.replace("%data%", "Grgrgrrr");
-var formatedRole = HTMLheaderRole.replace("%data%", "developer");
+
+var work = {}
+work.city = "Rostov";
+work.role = "Developer";
+
+var bio =
+{
+ "name" : "grgrgrrr",
+ "role" : "developer",
+ "contacts" : {
+     "mobile" : "lalala",
+     "country" : "Russia"},
+ "welcome" : "hello, bro",
+ "skills" : ["awesomeness", "funny"],
+ "picture" : "images/fry.jpg"
+}
+
+var project =
+{
+  "name" : "dressMe",
+  "data" : "2015"
+}
+
+var education =
+{
+ "schools": [{
+  "name": "56",
+  "city": "Rostov"
+ }, {
+  "name": "12",
+  "city": "Rostov"
+ }]
+}
+
+var formattedName = HTMLheaderName.replace("%data%", bio.name);
+var formatedRole = HTMLheaderRole.replace("%data%", work.role);
 $("#header").prepend(formattedName);
 $("#header").append(formatedRole);
+
+
+//$("#main").append(bio.name);
+//$("#main").append(bio["welcome"]);
