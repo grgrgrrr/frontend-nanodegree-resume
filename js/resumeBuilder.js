a26fc9d28128e336d607a585213e9ddf2e96cec6
Grgrgrrr <annr29@yandex.ru>
2016-03-15 13:34:50 +0000

    Header added

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,5 +1,7 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
-
- $("#main").append("peppa");
+var formattedName = HTMLheaderName.replace("%data%", "Grgrgrrr");
+var formatedRole = HTMLheaderRole.replace("%data%", "developer");
+$("#header").prepend(formattedName);
+$("#header").append(formatedRole);
