695df59238adfc9c5954e649dfcec005ab278c94
Grgrgrrr <annr29@yandex.ru>
2016-03-17 16:09:42 +0000

    Work experience added

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,20 +2,36 @@
 This is empty on purpose! Your code to build the resume will go here.
  */
 
-var work = {}
-work.city = "Rostov";
-work.role = "Developer";
+var work = {
+    jobs :
+        [
+            {
+                "city": "Rostov",
+                "role": "Developer",
+                "employer": "sva"
+            }  ,
+            {
+                "city": "Rostov",
+                "role": "Teacher",
+                "employer": "sfedu"
+            }
+        ]
+}
 
 var bio =
 {
- "name" : "grgrgrrr",
- "role" : "developer",
- "contacts" : {
-     "mobile" : "lalala",
-     "country" : "Russia"},
- "welcome" : "hello, bro",
- "skills" : ["awesomeness", "funny"],
- "picture" : "images/fry.jpg"
+    "name" : "grgrgrrr",
+    "role" : "developer",
+    "contacts" : {
+        "mobile" : "lalala",
+        "country" : "Russia",
+        "github" : "grgrgrrr",
+        "twitter" : "grgrgrrr",
+        "e-mail" : "grgrgrrr"
+    },
+    "welcome" : "hello, bro",
+    "skills" : ["awesomeness", "funny", "responsible"],
+    "picture" : "images/fry.jpg"
 }
 
 var project =
@@ -35,10 +51,45 @@ var education =
  }]
 }
 
-var formattedName = HTMLheaderName.replace("%data%", bio.name);
-var formatedRole = HTMLheaderRole.replace("%data%", work.role);
-$("#header").prepend(formattedName);
-$("#header").append(formatedRole);
+
+//adding biography
+
+    var formattedName = HTMLheaderName.replace("%data%", bio.name);
+    $("#header").append(formattedName);
+
+    var formatedRole = HTMLheaderRole.replace("%data%", bio.role);
+    $("#header").append(formatedRole);
+
+  /*  for (var i = 0; i < bio.contacts.length; i++)
+    {
+        $("#header").append(Html);
+        var formatedTitle =
+        var formattedContact =
+    }*/
+
+
+
+//adding skills
+if (bio["skills"].length > 0)
+{
+    $("#header").append(HTMLskillsStart);
+    for (var i =0; i<bio["skills"].length; i++)
+    {
+        var formattedSkill = HTMLskills.replace("%data%", bio.skills[i]);
+        $("#skills").append(formattedSkill);
+    }
+}
+
+//adding work experience
+for (var item in work["jobs"] )
+{
+    $("#workExperience").append(HTMLworkStart);
+    var formatTitle = HTMLworkTitle.replace("%data%", work.jobs[item].role);
+    var formatEmployer = HTMLworkEmployer.replace("%data%",  work.jobs[item].employer);
+    var concat = formatEmployer + formatTitle;
+    $(".work-entry:last").append(concat);
+}
+
 
 
 //$("#main").append(bio.name);
--- js/supportTasks.js
+++ js/supportTasks.js
@@ -24,3 +24,24 @@ function nameChanger(oldName) {
     // Don't delete this line!
     return finalName;
 };
+
+
+//replace all < > to "" to check harmless strings
+
+var html = '<script src="http://hackyourwebsite.com/eviljavascript.js"></script>';
+var charEscape = function(_html) {
+    var newHTML = _html;
+    // How will you make sure that newHTML doesn't contain any < or > ?
+    // Your code goes here!
+
+    var patternOpen = /</gi;
+    var patternClose = />/gi;
+    newHTML = newHTML.replace(patternOpen, "&lt;");
+    newHTML = newHTML.replace(patternClose, "&gt;");
+
+    // Don't delete this line!
+    return newHTML;
+};
+
+// Did your code work? The line below will tell you!
+console.log(charEscape(html));
